cd903bdc187fb92525f5610ee483686619184a56
MongoAurelio <bastard_racer@hotmail.com>
2023-12-28 22:59:04 -0300

    eventos en react

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,7 +2,14 @@ import './CreateTodoButton.css';
 
 function CreateTodoButton() {
     return (
-    <div className="CreateTodoButton">
+    <div className="CreateTodoButton"
+      onClick={
+          (event) => {
+            console.log('le diste click');
+            console.log(event);
+            console.log(event.target);
+          }
+        }>
         <button>+</button>
     </div>
     );
